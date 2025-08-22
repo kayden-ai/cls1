c72c8f66cb0ff596eb7641c45a1a509e1b920071
ayush shah <shahayush53627@gmail.com>
2025-08-22 15:30:12 +0300

    Add files via upload

---
+++ ayush.py
@@ -0,0 +1,8 @@
+marks1 = int(input("enter your marks in physics"))
+marks2 = int(input("enter your marks in maths"))
+marks3 = int(input("enter your marks in programming"))
+total = ((marks1 + marks2 + marks3)/225)*100
+int_total = int(total)
+
+print(total)
+print(int_total)
